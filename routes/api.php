2d9a1e5efa4676551a5c0ad5885e19d9f1e2a10d
KoenHoeijmakers <koen@hoeijmakers.me>
2019-05-14 11:55:41 +0200

    chore: small cleanups

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,12 @@ use Illuminate\Routing\Router;
 
 /** @var $router Illuminate\Routing\Router */
 
+// @formatter:off
 
+$router->namespace('Api')->prefix('api')->group(function (Router $router) {
+
+});
+
+$router->post('oauth/token', '\Laravel\Passport\Http\Controllers\AccessTokenController@issueToken')->name('passport.token');
+
+// @formatter:on
