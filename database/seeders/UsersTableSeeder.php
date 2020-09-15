047cd31fa0ac78ba0bd71cf76f5636b6d9e41919
Koen Hoeijmakers <koen@hoeijmakers.me>
2020-09-15 15:34:26 +0200

    chore: fix seeders

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -2,7 +2,10 @@
 
 declare(strict_types=1);
 
+namespace Database\Seeders;
+
 use App\Models\User;
+use Database\Factories\UserFactory;
 use Illuminate\Database\Seeder;
 
 final class UsersTableSeeder extends Seeder
@@ -10,7 +13,7 @@ final class UsersTableSeeder extends Seeder
     public function run(): void
     {
         if (User::query()->where('email', 'info@kingscode.nl')->doesntExist()) {
-            factory(User::class)->create([
+            UserFactory::new()->createOne([
                 'email' => 'info@kingscode.nl',
             ]);
         }
